41f0cdda00ff6cc93d4982d9782b5b39ebcde24c
Sarthak Madhamshettiwar <sarthakmadhamshettiwar@gmail.com>
2024-01-15 23:07:52 +0530

    Update helpers.py
    Some minor changes.

--- helpers.py
+++ helpers.py
@@ -46,7 +46,7 @@ class Auxilary_Functions:
         return response.text
 
 
-class CodeAnalyzer:
+class Code_Analyzer:
     def __init__(self, model, oAuthToken, userName, repoName, fileName, length=75):
         github_operator = Github_Operator(oAuthToken, userName, repoName, fileName)
         self.model = model
@@ -60,8 +60,8 @@ class CodeAnalyzer:
     def time_space_complexity_analyzer(self):
       prompt_template = PromptTemplate.from_template(
           '''
-          Find the time and space complexity of the following code. Answer in two points. First point should contain the exact 
-          time and space complexity without any explaination. While second point should contain the brief explaination of calculation of time and space complexity. 
+          Find the time and space complexity of the following code. Answer in two points. First point should contain the exact
+          time and space complexity without any explaination. While second point should contain the brief explaination of calculation of time and space complexity.
           {code}.
           '''
       )
@@ -83,10 +83,10 @@ class CodeAnalyzer:
         '''
         prompt_template = PromptTemplate.from_template(
             '''
-            Given the brief description of problem and its solution code. Try to optimize the code using methods like Dynamic Programming, 
-            Segment Trees, Binary Search instead of Linear Search, any 1D or 2D Precomputation or Binary Exponentiation to reduce time 
-            complexity. Also try to reduce space complexity by changing the data structure, ex: Using adjacency list instead of 
-            adjacency matrix and etc. Output the optimized code brief overview of the changes done.\. 
+            Given the brief description of problem and its solution code. Try to optimize the code using methods like Dynamic Programming,
+            Segment Trees, Binary Search instead of Linear Search, any 1D or 2D Precomputation or Binary Exponentiation to reduce time
+            complexity. Also try to reduce space complexity by changing the data structure, ex: Using adjacency list instead of
+            adjacency matrix and etc. Output the optimized code brief overview of the changes done.\.
             Description: {description}
             Code: {code}
             '''
@@ -106,11 +106,11 @@ class CodeAnalyzer:
         '''
         prompt_template = PromptTemplate.from_template(
             '''
-            Given the brief description and the code, update the above code in such a way that it follows KISS, DRY, YAGNI Principles. 
-            Also take care of abstraction and 
-            not to make any critical attributes or methods public. If similar sequence of operations are performed, make a 
-            new function for operation to suit DRY principle. Output the correct code and nothing more. Don't enclose 
-            code with backticks. 
+            Given the brief description and the code, update the above code in such a way that it follows KISS, DRY, YAGNI, SOLID Principles.
+            Also take care of abstraction,
+            don't make any critical attributes or methods public. If similar sequence of operations are performed in main program, make a
+            new function for operation to suit DRY principle. Output the correct code and nothing more. Don't enclose
+            code with backticks.
             Description: {description}
             Code: {code}
             '''
@@ -123,35 +123,6 @@ class CodeAnalyzer:
         modified_code = self.model.generate_content(prompt_parts)
         return modified_code.text
 
-    def bug_analyzer(self):
-        '''
-        :param description: brief overview of the code.
-        :param code: code, which we want to improve
-        :return: Will identify the possible bugs, like runtime or logic errors including infinite loops, etc.
-        '''
-        prompt_template = PromptTemplate.from_template(
-            '''
-            Given small description of problem and code written, check for logic errors and runtime errors. 
-            Runtimer erros include: out-of-bound error (accessing element that doesn't exist), integer overflow error, 
-            division by zero, trying to dereference NULL pointer, etc.
-            Logic errors: flawed logic, incorrect algorithmic implementation, early or delayed termination of recurssion or loops 
-            leading to infinite loops, etc. 
-            Pinpoint bugs with possible solutions or preventive measures. Only bugs and solutions are needed. No need for new code.
-            If you think code doesn't have any bugs print 'CORRECT CODE', or if you are not sure, then say so. 
-            Don't give wrong points please.
-            Description: {description}
-            Code: {code}
-            ALERT: Don't write updated code !!!
-            '''
-        )
-
-        # Modify this to generate content about any topic in any number of words. Level bhi choose kar sakte khud se.
-        prompt_parts = [
-            prompt_template.format(description=self.description, code=self.code)
-        ]
-        bugs = self.model.generate_content(prompt_parts)
-        return bugs.text
-
     def testCase_generator(self, testcase):
         '''
         :param testcase: some sample testcases along with their input.
@@ -161,7 +132,7 @@ class CodeAnalyzer:
             Given code and the description, generate stronger test cases for the given code. Some test cases are already given.
             Code: {code}.
 
-            Below testcases are in the format: 
+            Below testcases are in the format:
             'Input': .....
             'Output': .....
             Testcases: {testcase}
@@ -176,3 +147,33 @@ class CodeAnalyzer:
         ]
         testcases = self.model.generate_content(prompt_parts)
         return testcases.text
+
+
+    def bug_analyzer(self):
+        '''
+        :param description: brief overview of the code.
+        :param code: code, which we want to improve
+        :return: Will identify the possible bugs, like runtime or logic errors including infinite loops, etc.
+        '''
+        prompt_template = PromptTemplate.from_template(
+            '''
+            Given small description of problem and code written, check for logic errors and runtime errors.
+            Runtimer erros include: out-of-bound error (accessing element that doesn't exist), integer overflow error,
+            division by zero, trying to dereference NULL pointer, etc.
+            Logic errors: flawed logic, incorrect algorithmic implementation, early or delayed termination of recurssion or loops
+            leading to infinite loops, etc.
+            Pinpoint bugs with possible solutions or preventive measures. Only bugs and solutions are needed. No need for new code.
+            If you think code doesn't have any bugs print 'CORRECT CODE', or if you are not sure, then say so.
+            Don't give wrong points please.
+            Description: {description}
+            Code: {code}
+            ALERT: Don't write updated code !!!
+            '''
+        )
+
+        # Modify this to generate content about any topic in any number of words. Level bhi choose kar sakte khud se.
+        prompt_parts = [
+            prompt_template.format(description=self.description, code=self.code)
+        ]
+        bugs = self.model.generate_content(prompt_parts)
+        return bugs.text
