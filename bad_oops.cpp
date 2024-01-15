9e4f67258a468664a1a227fe65e32454816430f7
Sarthak Madhamshettiwar <sarthakmadhamshettiwar@gmail.com>
2024-01-15 22:54:29 +0530

    Create bad_oops.cpp
    A bad oops code.

---
+++ bad_oops.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Student{
+  public: 
+string name, address, usedId, password;
+};
+
+int main()
+{
+  Student s = new Student();
+  s.name = 'Sarthak';
+  s.password = 'github__';
+  return 0;
+}
